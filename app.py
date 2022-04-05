8a9d31915db0fb184fd9dda5d7e98c8e44c57201
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-04-05 16:49:13 +0200

    -refactoring: app and config files

---
+++ app.py
@@ -0,0 +1,7 @@
+from proteins.step1.PDB_info_extraction import pdb_info_extraction
+
+
+if __name__ == "__main__":
+    pdb_info_extraction()
+
+    print("first try")
---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    PROTEINS_FOLDER = os.path.join(basedir, "proteins\proteins_files")
+    RNA_FOLDER = os.path.join(basedir, "proteins\rna_files")
+    POLIMER_ENTITY_TYPE = "PROTEINS" or "RNA"
+    OUTPUT_FOLDER = os.path.join(basedir, "excel_files")
