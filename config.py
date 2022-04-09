be2d5afd7f6f3bc8717ec3b873c65a9c82605a52
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-04-09 03:01:05 +0200

    renamed rna to ribosome

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     PROTEINS_FOLDER = os.path.join(basedir, "proteins\proteins_files")
-    RNA_FOLDER = os.path.join(basedir, "proteins\rna_files")
+    RNA_FOLDER = os.path.join(basedir, "proteins\ribosome_files")
     POLIMER_ENTITY_TYPE = "PROTEINS" or "RNA"
     OUTPUT_FOLDER = os.path.join(basedir, "excel_files")
