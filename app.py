d4c9186ea64253600d3d7695d9c6408cf03ed09a
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-04-11 13:31:52 +0200

    Step3: completed!

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from proteins.step1.PDB_info_extraction import pdb_info_extraction
 from proteins.step2.exec_prepare_receptors import prepare_receptors
 from proteins.step2.split_chains import split_chains
+from proteins.step3.create_gridbox import create_gridbox
+
 
 if __name__ == "__main__":
     # pdb_info_extraction()
     # split_chains()
-    prepare_receptors()
+    # prepare_receptors()
+    create_gridbox()
