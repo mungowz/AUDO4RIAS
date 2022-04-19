cbac1c7e42e4d4818ccef61e8b45e344d41f06f6
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-04-19 18:48:46 +0200

    removed comments

--- proteins/step1/download_pdbs.py
+++ proteins/step1/download_pdbs.py
@@ -10,7 +10,6 @@ def download_pdbs(pdbs_list=[], output_path=None):
     # download only pdb files that aren't already downloaded
     for protein_code in pdbs_list:
 
-        #   print("Downloading pdb file for {code}...".format(code=protein_code))
         protein_file = protein_code + ".pdb"
 
         for root, dirs, files in os.walk(output_path):
@@ -37,5 +36,3 @@ def download_pdbs(pdbs_list=[], output_path=None):
                         code=protein_code, output_file=output_file
                     )
                 )
-
-            # print(ppdb.df)
