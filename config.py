806191ce836c7a1b38dbc87e936924a4d9f2d644
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-04-19 19:02:17 +0200

    modified polymer_type_entity variable name to query_type

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     PROTEINS_FOLDER = os.path.join(basedir, "proteins\proteins_files")
     RNA_FOLDER = os.path.join(basedir, "proteins\ribosome_files")
-    POLIMER_ENTITY_TYPE = "PROTEINS" or "RNA"
+    QUERY_TYPE = "PROTEINS" or "RIBOSOME"
     EXCEL_FOLDER = os.path.join(basedir, "excel_files")
     GRIDBOX_OUTPUT = os.path.join(basedir, "proteins\gridbox_output")
     PDBQT_PROTEINS_FOLDER = os.path.join(basedir, "proteins\pdbqt")
