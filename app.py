4f9461a49f09797ae967721449adb44de4c4412c
Maax <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-04-19 19:58:29 +0200

    added comments

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ from proteins.step1.PDB_info_extraction import pdb_info_extraction
 from proteins.step2.exec_prepare_receptors import prepare_receptors
 from proteins.step2.split_chains import split_chains
 from proteins.step3.create_gridbox import create_gridbox
-from ligands import sdf_to_pdb
-
 
 if __name__ == "__main__":
-    # pdb_info_extraction()
-    # split_chains()
-    # prepare_receptors()
-    #create_gridbox()
-    sdf_to_pdb()
+    pdb_info_extraction()
+    split_chains()
+    prepare_receptors()
+    create_gridbox()
