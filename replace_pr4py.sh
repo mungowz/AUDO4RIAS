5ef06966d34ec42c5fce5d458be7613d7e7b8379
gomax22 <massimiliano.giordanoorsini001@studenti.uniparthenope.it>
2022-05-21 11:54:26 +0200

    CHANGES:
    -added our version of prepare_receptor4.py
    -added comments to replace_pr4py.sh
    -fixed .gitignore

--- replace_pr4py.sh
+++ replace_pr4py.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
+
+# find dir that contains "prepare_receptor4.py"
 path=$(find /home -type d -name "Utilities24")
+
+# move our "prepare_receptor4.py" to dirpath
 mv "prepare_receptor4.py" "$path"
+
+# set execution flag to "prepare_receptor4.py" 
 file="${path}/prepare_receptor4.py"
 chmod a+x "$file"
