b75b0158681c11f4ad557c6b55164acf89dd10f0
mungowz <a.mungari@gmail.com>
2022-05-23 13:53:48 +0200

    fixed parenthesis problem with prepare_ligands.py

--- ligands/sdf_to_pdbqt.py
+++ ligands/sdf_to_pdbqt.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import call
 
 
 def sdf_to_pdbqt(sdf_folder, pdbqt_folder, verbose):
@@ -7,11 +8,11 @@ def sdf_to_pdbqt(sdf_folder, pdbqt_folder, verbose):
             ligand_name = sdf_file.path.split(os.sep)[-1].split(".")[0]
             pdbqt_path = pdbqt_folder + "/ligand_" + ligand_name + ".pdbqt"
 
-            command = "obabel " + sdf_file.path + " -O " + pdbqt_path
+            command = ('obabel', sdf_file.path, '-O', pdbqt_path)
+            call(command)
             if verbose:
                 print(
                     "{ligand_name} converted from sdf into pdbqt! (Stored in {output_file})\n".format(
                         ligand_name=ligand_name, output_file=pdbqt_path
                     )
                 )
-            os.system(command=command)
