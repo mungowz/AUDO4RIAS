79181cf0432414fa7137db0a4aa4d1f0e637597a
mungowz <a.mungari@gmail.com>
2022-08-20 12:50:12 +0200

    fixed change page

--- gui/controller.py
+++ gui/controller.py
@@ -7,28 +7,29 @@ class Controller(tk.Tk):
     
     def __init__(self):
         tk.Tk.__init__(self)
+        
+        self.container = tk.Frame(self)
+        self.container.pack(
+            side = "top", 
+            fill = "both", 
+            expand = True
+        )
+        self.container.grid_rowconfigure(
+            0, 
+            weight = 1
+        )
+        self.container.grid_columnconfigure(
+            0, 
+            weight = 1
+        )
+
         self.model = Model(self)
         self.raise_page(Start_page)
 
 
     def raise_page(self, page):
-        container = tk.Frame(self)
-        container.pack(
-            side = "top", 
-            fill = "both", 
-            expand = True
-        )
-        container.grid_rowconfigure(
-            0, 
-            weight = 1
-        )
-        container.grid_columnconfigure(
-            0, 
-            weight = 1
-        )
-        
         frame = page(
-            container, 
+            self.container, 
             self
         )
         frame.grid(
@@ -36,4 +37,5 @@ class Controller(tk.Tk):
             column = 0, 
             sticky ="nsew"
         )
-        self.model.show_frame(page)
+        page.tkraise(self)
+        #self.model.show_frame(page)
