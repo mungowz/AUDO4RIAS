eba10646b64f369cc7a3e669233c17fbb19ab420
mungowz <a.mungari@gmail.com>
2022-08-22 19:37:46 +0200

    fixed problem about change page

--- gui/view.py
+++ gui/view.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import font
 
 
 LARGEFONT =("Verdana", 35)
@@ -9,12 +8,13 @@ LARGEFONT =("Verdana", 35)
 class Button(tk.Frame):
 
 
-	def __init__(self, root, controller, button_caption, page_to_raise):
+	def __init__(self, root, controller, button_caption, page_to_raise, button_row):
 		tk.Frame.__init__(self, root)
 		self.root = root
 		self.controller = controller
 		self.button_caption = button_caption
 		self.page_to_raise = page_to_raise
+		self.button_row = button_row
 		self.main()
 
 
@@ -23,20 +23,26 @@ class Button(tk.Frame):
 			self.root,
 			text=self.button_caption,
 			command=lambda : self.controller.raise_page(self.page_to_raise) 
+		)	
+		button.grid(
+			row = self.button_row, 
+			column = 1, 
+			padx = 10, 
+			pady = 10
 		)
 		button.pack()
-
 
 
 class Label(tk.Frame):
 
 
-	def __init__(self, root, controller, label_caption, label_font):
+	def __init__(self, root, controller, label_caption, label_font, label_row):
 		tk.Frame.__init__(self, root)
 		self.root = root
 		self.controller = controller
 		self.label_caption = label_caption
 		self.label_font = label_font
+		self.label_row = label_row
 		self.main()
 
 
@@ -47,7 +53,7 @@ class Label(tk.Frame):
 			font=self.label_font
 		)
 		label.grid(
-			row=0, 
+			row=self.label_row, 
 			column=4, 
 			padx=10, 
 			pady=10
@@ -60,6 +66,9 @@ class Start_page(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+
+		self.label_row = 1
+		self.button_row = 1
 		self.controller = controller
 		self.main()
 		
@@ -69,22 +78,28 @@ class Start_page(tk.Frame):
 			self, 
 			self.controller,
 			'Select one button', 
-			LARGEFONT
+			LARGEFONT,
+			self.label_row
 		)
+		self.label_row += 1
 
 		Button(
 			self,
 			self.controller,
 			'Preparation', 
-			Preparation
+			Preparation,
+			self.button_row
 		)
+		self.button_row +=1
 
 		Button(
 			self, 
 			self.controller,
 			'Docking',
-			Docking
+			Docking,
+			self.button_row
 		)
+		self.button_row +=1
 		
 
 class Preparation(tk.Frame):
@@ -92,6 +107,9 @@ class Preparation(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+
+		self.label_row = 1
+		self.button_row = 1
 		self.controller = controller
 		self.main()
 		
@@ -101,22 +119,28 @@ class Preparation(tk.Frame):
 			self, 
 			self.controller,
 			'Select one button', 
-			LARGEFONT
+			LARGEFONT,
+			self.label_row
 		)
+		self.label_row +=1
 
 		Button(
 			self,
 			self.controller, 
 			'Ligands', 
-			Ligands
+			Ligands,
+			self.button_row
 		)
+		self.button_row +=1
 		
 		Button(
 			self,
 			self.controller, 
 			'Receptors', 
-			Receptors
+			Receptors,
+			self.button_row
 		)
+		self.button_row +=1
 		
 
 class Ligands(tk.Frame):
@@ -124,6 +148,9 @@ class Ligands(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+
+		self.label_row = 1
+		self.button_row = 1
 		self.controller = controller
 
 
@@ -136,6 +163,9 @@ class Receptors(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+
+		self.label_row = 1
+		self.button_row = 1
 		self.controller = controller
 		
 
@@ -148,6 +178,9 @@ class Docking(tk.Frame):
 	
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
+
+		self.label_row = 1
+		self.button_row = 1
 		self.controller = controller
 		
 
