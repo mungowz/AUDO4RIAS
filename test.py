c4605c1ee00c103b881ca58d8f9e681de98b0fb4
mungowz <a.mungari@gmail.com>
2022-11-21 22:38:32 +0100

    deleted test file: test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-import subprocess, shlex
-
-command = "xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T whatever -e python3 performDocking.py"
-args = shlex.split(command)
-print(args)
-
-subprocess.Popen(args)
